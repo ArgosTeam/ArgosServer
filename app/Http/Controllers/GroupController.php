9d690893158ccc05e4d81dc7c0cf970b6f7580c8
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-01-27 20:57:05 +1100

    patch groupcontroller

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -4,9 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Classes\GroupFunctions;
 use Illuminate\Http\Request;
-
-
 use App\Http\Requests;
+use App\Models\User;
 
 class GroupController extends Controller
 {
