ab5dd51fd4be6cf44a5ae0b97e4eda26004eaa4c
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-01-28 00:13:46 +1100

    implement test

--- app/Classes/GroupFunctions.php
+++ app/Classes/GroupFunctions.php
@@ -55,9 +55,9 @@ class GroupFunctions
     }
 
     public static function accept($currentUser, $user_id, $group_id) {
-        $group = Group::leftJoin('group_user', function ($join) {
+        $group = Group::join('group_user', function ($join) {
             $join->on('groups.id', '=', 'group_user.group_id')
-                ->where('group_user.user_id', '=', $currentUser->id)
+                ->where('group_user.user_id', '=', $currentUser->id);
                 })
                ->find($group_id);
         $userToAccept = User::find($user_id);
