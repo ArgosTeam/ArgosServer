775c142fad03a4be4e0831520463854a3e99e712
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-02-01 03:10:36 +1100

    add test /search/events

--- tests/SearchTest.php
+++ tests/SearchTest.php
@@ -21,13 +21,34 @@ class SearchTest extends TestCase
                                     'grant_type' => 'password',
                                     'client_id' => '1',
                                     'client_secret' => '8KD1qlhGoguCBCTZDgWsRtV1cU6OZtRrsOJT0cjb',
-                                    'username' => 'aure.girard@gmail.com',
+                                    'username' => 'aure.girardeau@gmail.com',
                                     'password' => 'toto',
                                     'scope' => '*'
                                 ]);
         $token = json_decode($tokenResponse->getContent(), true);
         $response = $this->call('GET',
                                 '/api/search/contacts',[
+                                    'name_begin' => '',
+                                    'known_only' => false
+                                ],[],[], ['HTTP_Authorization' => 'Bearer ' . $token['access_token']]);
+        print_r($response->getContent());
+        $this->assertEquals(200, $response->status());
+    }
+
+    public function testEvents() {
+        $tokenResponse = $this->call('POST',
+                                '/oauth/token',
+                                [
+                                    'grant_type' => 'password',
+                                    'client_id' => '1',
+                                    'client_secret' => '8KD1qlhGoguCBCTZDgWsRtV1cU6OZtRrsOJT0cjb',
+                                    'username' => 'aure.girard@gmail.com',
+                                    'password' => 'toto',
+                                    'scope' => '*'
+                                ]);
+        $token = json_decode($tokenResponse->getContent(), true);
+        $response = $this->call('GET',
+                                '/api/search/events',[
                                     'name_begin' => '',
                                     'known_only' => false
                                 ],[],[], ['HTTP_Authorization' => 'Bearer ' . $token['access_token']]);
