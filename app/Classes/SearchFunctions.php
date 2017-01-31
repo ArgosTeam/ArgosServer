858e81e15b8600ccd3b6674c52e0286aa81addd6
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-02-01 04:08:42 +1100

    update user model to access frien

--- app/Classes/SearchFunctions.php
+++ app/Classes/SearchFunctions.php
@@ -39,6 +39,7 @@ class SearchFunctions {
     
     public static function  getContacts($currentUser, $nameBegin, $knownOnly) {
         $users = SearchFunctions::getUsers($currentUser, $nameBegin, $knownOnly);
+        Log::info('users    : ' . print_r($users, true));
         $groups =  Group::where('name', 'like', $nameBegin . '%')
                 ->limit(15)
                 ->get();
