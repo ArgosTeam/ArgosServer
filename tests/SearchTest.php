37df3c316a0b719ee3d28ddca6011295b302d1f1
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-02-01 02:05:08 +1100

    test contacts

--- tests/SearchTest.php
+++ tests/SearchTest.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -28,7 +29,7 @@ class SearchTest extends TestCase
         $response = $this->call('GET',
                                 '/api/search/contacts',[
                                     'name_begin' => '',
-                                    'known_only' => false
+                                    'known_only' => true
                                 ],[],[], ['HTTP_Authorization' => 'Bearer ' . $token['access_token']]);
         print_r($response->getContent());
         $this->assertEquals(200, $response->status());
