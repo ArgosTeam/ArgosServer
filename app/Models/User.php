0d623ac2303a2070f67c2b88d50b0a2dd69f9268
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-02-01 04:04:25 +1100

    update user model to access frien

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,6 +44,6 @@ class User extends Authenticatable
     }
 
     public function friends() {
-        return $this->belongsTo(Friend::class, 'user_users');
+        return $this->belongsTo(Friend::class);
     }
 }
