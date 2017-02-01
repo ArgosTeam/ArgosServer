d936df76f03ca09711a9aeb7f1d4e4db17a4b62e
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-02-02 02:07:12 +1100

    events tests

--- tests/SearchTest.php
+++ tests/SearchTest.php
@@ -50,7 +50,7 @@ class SearchTest extends TestCase
         $response = $this->call('GET',
                                 '/api/search/events',[
                                     'name_begin' => '',
-                                    'known_only' => false
+                                    'known_only' => true
                                 ],[],[], ['HTTP_Authorization' => 'Bearer ' . $token['access_token']]);
         print_r($response->getContent());
         $this->assertEquals(200, $response->status());
