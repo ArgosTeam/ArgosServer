f9f57967377c39b79edc4f73acd6f73eed769e49
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-02-03 18:22:33 +1100

    join before select user infos

--- app/Classes/UserFunctions.php
+++ app/Classes/UserFunctions.php
@@ -58,7 +58,7 @@ class UserFunctions
 
         $user = User::select('users.*', 'user_users.own', 'user_users.active')
               ->leftJoin('user_users', 'users.id', '=', 'user_users.user_id')
-              ->where('user_users.friend_id', '=', [$user->id, null])
+              //->where('user_users.friend_id', '=', [$user->id, null])
               ->where('users.id', '=', $id)
               ->first();
         Log::info('DEBUG : ' . print_r($user, true));
