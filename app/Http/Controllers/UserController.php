3518bb6662f47933ce2dd00c0baf25d1543b06c1
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-02-03 18:14:23 +1100

    missing Auth Cpmponent in User Controller patche

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Classes\UserFunctions;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests;
 
 class UserController extends Controller
