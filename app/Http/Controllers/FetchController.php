10b112fc15c40cda6b391550cc18d48a5002eb9b
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-02-08 02:31:40 +1100

    patch photo and fetch

--- app/Http/Controllers/FetchController.php
+++ app/Http/Controllers/FetchController.php
@@ -6,12 +6,14 @@ use App\Classes\fetchFunctions;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Log;
 
 class FetchController extends Controller
 {
 
     public function fetch(Request $request) {
-        $data = $request->input();;
+        $data = $request->input();
+        Log::info('LOOOOG : ' . print_r($data, true));
         return response(fetchFunctions::fetch($data));
     }
 }
