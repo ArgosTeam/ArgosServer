253c10ca7ee9176c2d5718a5dc1e9cc34eee8d52
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-02-07 20:13:35 +1100

    add file

---
+++ app/Models/Comment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $fillable = ['user_id',
+                           'content',
+                           'created_at',
+                           'updated_at'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function events() {
+        return $this->belongsToMany(Event::class)
+            ->withTimestamps();;
+    }
+
+    public function photos() {
+        return $this->belongsToMany(Photo::class)
+            ->withTimestamps();
+    }
+}
