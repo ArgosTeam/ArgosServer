4c9faf36013f9ffa53b4d47294b96c3cee955fa3
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-02-13 21:10:35 +1100

    upload test fetch in PhotoTest

--- tests/PhotoTest.php
+++ tests/PhotoTest.php
@@ -33,9 +33,9 @@ class   PhotoTest extends TestCase
                         'farLeft' => 'LatLng(54.38655404338235, -7.254924531250026)',
                         'nearLeft' => 'LatLng(52.2879010895274, -7.254924531250026)',
                         'nearRight' => 'LatLng(52.2879010895274, -5.277385468750026)',
-                        'hashtag' => null,
-                        'userId' => null,
-                        'groupId' => null
+                        'users' => [],
+                        'hashtags' => [],
+                        'groups' => []
                     ], [], [],
                     ['HTTP_Authorization' => 'Bearer ' . $token['access_token']]);
         print_r($response->getContent());
