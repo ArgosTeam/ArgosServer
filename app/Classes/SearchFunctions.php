62ecfaa1c30575ca610b9020071b5f6440cc5f1d
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-02-16 00:15:44 +1100

    patch search hashtag

--- app/Classes/SearchFunctions.php
+++ app/Classes/SearchFunctions.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use App\Models\Group;
 use App\Models\Event;
 use App\Models\Friend;
+use App\Models\Hashtag;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Log;
 
