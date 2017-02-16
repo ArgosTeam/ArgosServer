044ac86e5b8041b5864b1e6463ebd14238b2138c
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-02-16 17:56:05 +1100

    add Log to Users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Classes\UserFunctions;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use App\Http\Requests;
 
 class UserController extends Controller
