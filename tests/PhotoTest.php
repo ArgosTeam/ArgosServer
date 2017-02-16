0ff119d53d5c50bfa61b7d2a97bba6414fb3b1a9
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-02-16 17:55:21 +1100

    add UploadedFile dependency to PhotoTest

--- tests/PhotoTest.php
+++ tests/PhotoTest.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Http\UploadedFile;
 
 class   PhotoTest extends TestCase
 {
