e9a285d016b062a60579a32bdf19339f73d91750
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-02-17 19:45:35 +1100

    add upload test from user, geolocalised photo

--- tests/PhotoTest.php
+++ tests/PhotoTest.php
@@ -43,4 +43,42 @@ class   PhotoTest extends TestCase
         print_r($response->getContent());
         $this->assertEquals(200, $response->status());
     }
+
+    public function testUpload() {
+        //  Get token        
+        $tokenResponse = $this->call('POST',
+                                     '/oauth/token',
+                                     [
+                                         'grant_type' => 'password',
+                                         'client_id' => '1',
+                                         'client_secret' => '8KD1qlhGoguCBCTZDgWsRtV1cU6OZtRrsOJT0cjb',
+                                         'username' => 'aure.girardeau@gmail.com',
+                                         'password' => 'toto',
+                                         'scope' => '*'
+                                     ]);
+        $token = json_decode($tokenResponse->getContent(), true);
+
+        $path = __DIR__ . '/images/test_upload.jpeg';
+        $type = pathinfo($path, PATHINFO_EXTENSION);
+        $data = file_get_contents($path);
+        $base64 = base64_encode($data);
+
+        // Test upload
+        $response = $this->call('POST',
+                    '/api/photo/upload',
+                    [
+                        'image' => $base64,
+                        'description' => 'Ceci est une photo test upload',
+                        'latitude' => -37.8267193,
+                        'longitude' => 144.9592682,
+                        'hashtags' => [
+                            '#tropfrais',
+                            '#arthurlapute'
+                        ],
+                        'mode' => 'normal'
+                    ], [], [],
+                    ['HTTP_Authorization' => 'Bearer ' . $token['access_token']]);
+        print_r($response->getContent());
+        $this->assertEquals(200, $response->status());
+    }
 }
