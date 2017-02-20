2f9ae16c556df14cbfdeaf3261acd003f9833071
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-02-21 01:54:27 +1100

    add GetNotifs test

---
+++ tests/NotificationTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class NotificationTest extends TestCase {
+    function setUp() {
+        parent::setUp();
+    }
+
+    public function testGetNotifs() {
+        $tokenResponse = $this->call('POST',
+                                     '/oauth/token',
+                                     [
+                                         'grant_type' => 'password',
+                                         'client_id' => '2',
+                                         'client_secret' => 'H9c9USUmSWsw2yxqxrnPbXl8sPvRfDCxztFc7xZ8',
+                                         'username' => 'aure.girard@gmail.com',
+                                         'password' => 'toto',
+                                         'scope' => '*'
+                                     ]);
+        $token = json_decode($tokenResponse->getContent(), true);
+
+        
+        $response = $this->call('GET',
+                                '/api/notifs',
+                                [], [], [],
+                                ['HTTP_Authorization' => 'Bearer ' . $token['access_token']]);
+        print_r($response->getContent(), true);
+        $this->assertEquals(200, $response->status());
+    }
+}
