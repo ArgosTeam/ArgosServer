947cc4f715b79aaf6386e76e77f5aeb757f53fb0
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-02-21 02:35:06 +1100

    add mark read test

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
-use IlluminateSupport\Facades\\Notification;
+use Illuminate\Support\Facades\Notification;
 
 class NotificationController extends Controller
 {
