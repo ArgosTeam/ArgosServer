c238134b281dea3a0da3dbcd5799582b11a4ef78
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-02-23 23:53:00 +1100

    update testSearch

--- tests/SearchTest.php
+++ tests/SearchTest.php
@@ -28,7 +28,7 @@ class SearchTest extends TestCase
         $token = json_decode($tokenResponse->getContent(), true);
         $response = $this->call('GET',
                                 '/api/search/contacts',[
-                                    'name_begin' => 'a',
+                                    'name_begin' => '',
                                     'known_only' => false,
                                     'id' => -1
                                 ],[],[], ['HTTP_Authorization' => 'Bearer ' . $token['access_token']]);
