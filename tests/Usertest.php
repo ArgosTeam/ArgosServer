9fe0a11ab4f309f1139d8cde9857a7ead2ff0637
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-03-14 16:26:15 +1100

    add Usertest->session

--- tests/Usertest.php
+++ tests/Usertest.php
@@ -114,4 +114,27 @@ class   UserTest extends TestCase
         print_r($response->getContent());
         $this->assertEquals(200, $response->status());
     }
+
+    public function testSession() {
+        $tokenResponse = $this->call('POST',
+                                     '/oauth/token',
+                                     [
+                                         'grant_type' => 'password',
+                                         'client_id' => '2',
+                                         'client_secret' => 'H9c9USUmSWsw2yxqxrnPbXl8sPvRfDCxztFc7xZ8',
+                                         'username' => 'aure.girard@gmail.com',
+                                         'password' => 'toto',
+                                         'scope' => '*'
+                                     ]);
+        $token = json_decode($tokenResponse->getContent(), true);
+
+        // Test album
+        $response = $this->call('GET',
+                    '/api/user/session',
+                    [
+                    ], [], [],
+                    ['HTTP_Authorization' => 'Bearer ' . $token['access_token']]);
+        print_r($response->getContent());
+        $this->assertEquals(200, $response->status());
+    }
 }
