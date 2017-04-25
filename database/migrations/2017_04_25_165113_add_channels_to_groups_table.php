2febf1e96d26a836f1986acc171a6979d594aa50
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-04-25 18:57:39 +0200

    add new migrations to handle messenger with channels

---
+++ database/migrations/2017_04_25_165113_add_channels_to_groups_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddChannelsToGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('groups', function (Blueprint $table) {
+            $table->integer('channel_id')->unsigned();
+
+            $table->foreign('channel_id')
+                ->references('id')
+                ->on('channels')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('groups', function (Blueprint $table) {
+            //
+        });
+    }
+}
