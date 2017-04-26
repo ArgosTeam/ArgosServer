a00b142799b3b4380df24fd5f8988a55cfd31ea1
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-04-26 20:18:39 +0200

    patch routes

--- routes/api.php
+++ routes/api.php
@@ -163,8 +163,8 @@ Route::group(['middleware' => 'auth:api'], function () {
 
     /* POST */
     Route::post('/chat/user', 'MessengerController@sendToUser');
-    Route::post('/chat/group', 'MessengerController@sendToGroup');
-    Route::post('/chat/event', 'MessengerController@sendToEvent');
+    Route::post('/chat/group', 'MessengerController@sendInGroup');
+    Route::post('/chat/event', 'MessengerController@sendInEvent');
 
     /* GET */
     Route::get('/user/messages', 'MessengerController@getUserMessages');
