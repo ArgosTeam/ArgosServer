d69f1fe718eced5e375d89af7e4d1f76d4599b6d
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-05-16 16:51:21 +0200

    add mode in fetch

--- app/Classes/fetchFunctions.php
+++ app/Classes/fetchFunctions.php
@@ -220,6 +220,7 @@ class fetchFunctions
                                 'path' => $path,
                                 'lat' => $location->lat,
                                 'lng' => $location->lng,
+                                'mode' => $photo->mode,
                                 'photos' => []
                             ];
                             $index += 1;
@@ -230,6 +231,7 @@ class fetchFunctions
                                 'path' => $path,
                                 'lat' => $location->lat,
                                 'lng' => $location->lng,
+                                'mode' => $photo->mode
                             ];
                         }
                         $main = false;
