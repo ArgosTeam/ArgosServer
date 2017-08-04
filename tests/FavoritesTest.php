bae6f014842975f0cdb06a8e2a344fcc9efc6914
Aurélien Girardeau <aure.girardeau@gmail.com>
2017-08-04 12:11:31 +0200

    add test

---
+++ tests/FavoritesTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class FavoritesTest extends TestCase {
+
+    function setUp() {
+        parent::setUp();
+    }
+
+    public function testFavorites() {
+        $tokenResponse = $this->call('POST',
+                                     '/oauth/token',
+                                     [
+                                         'grant_type' => 'password',
+                                         'client_id' => '2',
+                                         'client_secret' => 'BwWvV7ecYlc42uGhBRojBWEHMtqfZeyKEJz5kVw0',
+                                         'username' => 'aure.girardeau@gmail.com',
+                                         'password' => 'toto',
+                                         'scope' => '*'
+                                     ]);
+        $token = json_decode($tokenResponse->getContent(), true);
+
+        
+        $response = $this->call('GET',
+                                '/api/friend/favorites',
+                                [], [], [],
+                                ['HTTP_Authorization' => 'Bearer ' . $token['access_token']]);
+
+
+        print_r($response->getContent());
+        $this->assertEquals(200, $response->status());
+    }
+}
